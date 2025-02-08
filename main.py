65a4d82529d87294096bbcc84ddd2a7ba653f7f0
FOLASAYO SAMUEL OLAYEMI <61091636+Folasayo-Samuel@users.noreply.github.com>
2025-02-08 03:14:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,103 @@
 from fastapi import FastAPI
-import os
-from pymongo import MongoClient
-from dotenv import load_dotenv
-from pymongo.encryption_options import AutoEncryptionOpts
-from pymongo.encryption import ClientEncryption
+from pydantic import BaseModel
 from bson.codec_options import CodecOptions
-from bson.binary import STANDARD
-
-load_dotenv()
-
+from pymongo import MongoClient
+from pymongo.encryption import Algorithm, ClientEncryption, QueryType
+from pymongo.encryption_options import AutoEncryptionOpts
+import os
 
 app = FastAPI()
 
-kms_provider_name = "local"
-
-
-uri = os.getenv("MONGODB_URI")
-
-key_vault_database_name = "encryption"
-key_vault_collection_name = "__keyVault"
-key_vault_namespace = f"{key_vault_database_name}.{key_vault_collection_name}"
-encrypted_database_name = "medicalRecords"
-encrypted_collection_name = "patients"
-
-# Create a Customer Master Key. You must create a Customer Master Key (CMK) to perform Queryable Encryption.
-path = "customer-master-key.txt"
-file_bytes = os.urandom(96)
-with open(path, "wb") as file:
-    file.write(file_bytes)
-    
-path = "./customer-master-key.txt"
-with open(path, "rb") as file:
-    local_master_key = file.read()
-    kms_provider_credentials = {
-        "local": {
-            "key": local_master_key
-        },
-    }
-
-# The path to your Automatic Encryption Shared Library
-auto_encryption_options = AutoEncryptionOpts(
-    kms_provider_credentials,
-    key_vault_namespace,
-    # crypt_shared_lib_path=shared_lib_path, 
-    # Path to your Automatic Encryption Shared Library
-    mongocryptd_uri=os.getenv("mongodb://localhost:27020"),
-    mongocryptd_bypass_spawn=False,
-    mongocryptd_spawn_path="mongocryptd",
-)
-
-# Create a Client to Set Up an Encrypted Collection
-encrypted_client = MongoClient(uri, auto_encryption_opts=auto_encryption_options)
-
-# Specify Fields to Encrypt
-encrypted_fields_map = {
-    "fields": [
-        {
-            "path": "patientRecord.ssn",
-            "bsonType": "string",
-            "queries": [{"queryType": "equality"}]
-        },
-        {
-            "path": "patientRecord.billing",
-            "bsonType": "object",
-        }
-    ]
-}
+local_master_key = os.urandom(96)
+kms_providers = {"local": {"key": local_master_key}}
+key_vault_namespace = "keyvault.datakeys"
+mongodb_url="mongodb+srv://<user's-database-name>:<user's-database-password>@mycluster.wa2cj.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster"
+key_vault_client = MongoClient(mongodb_url)
 
 client_encryption = ClientEncryption(
-    kms_providers=kms_provider_name,
-    key_vault_namespace=key_vault_namespace,
-    key_vault_client=encrypted_client,
-    codec_options=CodecOptions(uuid_representation=STANDARD)
+    kms_providers, key_vault_namespace, key_vault_client, CodecOptions()
 )
 
-customer_master_key_credentials = {"local": {"key": local_master_key}}
+key_vault = key_vault_client["keyvault"]["datakeys"]
+key_vault.drop()
 
-client_encryption.create_encrypted_collection(
-    encrypted_client[encrypted_database_name],
-    encrypted_collection_name,
-    encrypted_fields_map,
-    kms_provider_credentials, 
-    customer_master_key_credentials,
+# Ensure that two data keys cannot share the same keyAltName.
+key_vault.create_index(
+    "keyAltNames",
+    unique=True,
+    partialFilterExpression={"keyAltNames": {"$exists": True}},
 )
 
-encrypted_collection = encrypted_client[encrypted_database_name][encrypted_collection_name]
-@app.post("/user")
-async def create_user(patientName: str, patientId: int, patientRecord: object):
-    insert_id = encrypted_collection.insert_one({"patientName": patientName, "patientId": patientId, "patientRecord": patientRecord}).inserted_id
-    return {"message": "User created successfully", "insert_id": insert_id}
+key1_id = client_encryption.create_data_key("local", key_alt_names=["firstName"])
+key2_id = client_encryption.create_data_key("local", key_alt_names=["lastName"])
+key3_id = client_encryption.create_data_key("local", key_alt_names=["passportNumber"])
+
+encrypted_fields_map = {
+    "default.encryptedCollection": {
+        "fields": [
+            {
+             	"path": "firstName",
+                "bsonType": "string",
+                "keyId": key1_id,
+                "queries": [{"queryType": "equality"}],
+            },
+            {
+             	"path": "lastName",
+                "bsonType": "string",
+                "keyId": key2_id,
+                 "queries": [{"queryType": "equality"}],
+
+            },
+            {
+
+              "path": "passportNumber",
+              "bsonType": "string",
+              "keyId": key3_id,
+              "queries": [{"queryType": "equality"}]
+            },
+	],
+    }
+}
+auto_encryption_opts = AutoEncryptionOpts(
+    kms_providers,
+    key_vault_namespace,
+    encrypted_fields_map=encrypted_fields_map,
+    crypt_shared_lib_path="/usr/local/lib/mongo_crypt_v1.so"
+
+)
+
+print(f"Auto Encryption Options: {auto_encryption_opts}")
+
+client = MongoClient(mongodb_url,auto_encryption_opts=auto_encryption_opts)
+client.default.drop_collection("encryptedCollection")
+coll = client.default.create_collection("encryptedCollection")
+
+print(f"MongoClient Issue: {client}")
+print(f"Encrypted Conllection: {coll}")
+
+class User(BaseModel):
+    firstName: str
+    lastName: str
+    passportNumber: str
+
+def user_helper(user: dict) -> dict:
+    return {
+         "id": str(user["_id"]),
+         "fisrtName": user["firstName"],
+         "lastName": user["lastName"],
+         "passportNumber": user["passportNumber"]
+}
+
+
+@app.get("/get-user/{passportNumber}")
+def get_user(passport_number: str):
+    fetch_user = coll.find_one({"passportNumber": passport_number})
+    if fetch_user:
+        return user_helper(fetch_user)
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.post("/create-user")
+def create_user(user: User):
+    print(user)
+    insert_result = coll.insert_one(user.dict())
+    return {"message":"Document inserted successfully", "insert_id":str(insert_result.inserted_id)}
